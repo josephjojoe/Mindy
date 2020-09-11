cf0fff1a95b87a66b8e45769c4a27c941892e754
Joseph Jojoe <joseph.jojoe101@gmail.com>
2020-09-11 21:07:17 +0100

    Add files via upload
    Added Bot activity status framework code - needs working on to make it Mindy-specific.

--- bot.py
+++ bot.py
@@ -21,6 +21,16 @@ bot.load_extension('links')
 @bot.event
 async def on_ready():
     print("Mindy is ready.")
+
+# Sets bot activity status
+@bot.command()
+async def status_watcher(ctx):
+    game = discord.Game("with the API")
+    await bot.change_presence(activity=game)
+
+@bot.command()
+async def servercount(ctx):
+    await ctx.send(f"Connected to {len(bot.guilds)} servers.")
     
 # Runs bot with token
 bot.run(token)
